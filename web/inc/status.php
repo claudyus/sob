f5bfe2871165b888aa0db3346d1cb7a516c47f7d
Claudio Mignanti <c.mignanti@gmail.com>
2009-10-11 13:18:28 +0200

    webif: license also php files!

--- web/inc/status.php
+++ web/inc/status.php
@@ -1,4 +1,13 @@
 <?
+/* sOB - Simple Open builing Bot
+ *
+ * Copyright 2009 - Claudio Mignanti <c.mignanti@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+*/
 function status(){
 
 	$r = file ("../log/STATUS");

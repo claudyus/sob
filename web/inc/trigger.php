fbd15a9b435e29dcbba7ee5e7885b56fb1af67a6
Claudio Mignanti <c.mignanti@gmail.com>
2009-10-11 13:25:55 +0200

    webif: fix trigger.php and add copyright notice

--- web/inc/trigger.php
+++ web/inc/trigger.php
@@ -1,14 +1,25 @@
 <?
 
+/* sOB - Simple Open builing Bot
+ *
+ * Copyright 2009 - Claudio Mignanti <c.mignanti@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+*/
+
 function loader(){
 
 
 if (isset($_GET["target"])) {
-	exec("echo $_GET[\"target\"] >> /tmp/sob-planned");
-	$r .= "Target $_GET[\"target\"] compilation will start in less than 5 min";
+	exec("echo ".$_GET["target"]." >> /tmp/sob-planned");
+	$r .= "Target ".$_GET["target"]." compilation will start in less than 5 mins<br><br>";
 }
 
 $r .="Select target to re-run:<br>";
+
 
 exec("ls ../conf/*.conf -1 | cut -f 3 -d  / | cut -f 1 -d .", $shell);
 
@@ -16,5 +27,6 @@ foreach ($shell as $o) {
 	$r .= "<a href=\"?page=trigger&target=$o\">$o</a><br>\n";
 }
 
+return $r;
 }
 ?>
